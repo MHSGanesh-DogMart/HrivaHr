968fc47fcde8ba7a5f10958e0ac41d098d60a882
Hemanth <hemanth@mtouchlabs.com>
2026-04-08 10:40:37 +0530

    Add root landing route for diagnostics

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('HrivaHR Backend is Live 🚀');
+});
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
